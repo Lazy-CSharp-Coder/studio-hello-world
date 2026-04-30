04c6a05b93b6049cd2cd73c0d852943fee97836f
Harald <haraldn.kodeloftet@gmail.com>
2026-04-30 18:34:49 +0200

    tset

---
+++ schemaTypes/seriesType.ts
@@ -0,0 +1,73 @@
+import { defineField, defineType } from 'sanity';
+
+export const bookType = defineType({
+  name: 'Serie',
+  title: 'Serie',
+  type: 'document',
+  fields: [
+   
+    defineField({
+      name: 'seriesName',
+      type: 'string',
+      title: "Serie Title",
+      validation: (rule) => rule.required(),
+
+    }),
+     defineField({
+      name: 'uid',
+      type: 'string',
+      title: 'Unique Identifier',
+      validation: (rule) => rule.required(),
+    }),
+   
+ defineField({
+      name: 'authorUids',
+      type: 'array',
+      title: "Authors (identifier)",
+      of: [{ type: 'string' }],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'imageSeries',
+      type: 'image',
+      title: "Series general image",
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'seriesFact',
+      type: 'string',
+      title: "Fact about series.",
+    
+      validation: (rule) => rule.required(),
+    }),
+   
+
+    defineField({
+      name: 'seriesTeaser',
+      type: 'string',
+      title: "Teaser for series",
+      validation: (rule) => rule.required(),
+
+    }),
+    defineField({
+      name: 'description',
+      type: 'array',
+      of: [{ type: 'string' }],
+      validation: (rule) => rule.required(),
+    }),
+
+    defineField({
+      name: 'ageGroup',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'bookUids',
+      type: 'array',
+      of: [{ type: 'string' }],
+      validation: (rule) => rule.required(),
+    }),
+
+
+  ],
+});
